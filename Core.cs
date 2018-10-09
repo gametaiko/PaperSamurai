cce4d0303ccf1d5df4a63b9fed6f41f8db88dd61
Zainal <zainal@gametaiko.com>
2018-10-09 15:48:19 +0800

    Basic function of core

---
+++ Core.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour {
+
+    static public string name;
+    static public string telephone;
+    static public bool isBoss;
+    static public int health;
+    static public int score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
